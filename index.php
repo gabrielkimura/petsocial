12f988f0babd98aa0e37174acffe0209ad8b9bc1
gabrielkimura <gabrielkimura7@gmal.com>
2024-07-20 17:29:01 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>desaparecidos</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/menu-lateral.css">
+</head>
+<body>
+    <div class="sidebar">
+        <div class = "logo-item">
+            <img src="assets/img/logo.png" alt="desaparecidos">
+        </div>
+        <div class="menu-item">
+            <img src="assets/img/desaparecidos.png" alt="desaparecidos">
+            <a href="#">Desaparecidos</a>
+        </div>
+        <div class="menu-item">
+            <img src="assets/img/apoie.png" alt="ajude">
+            <a href="#">Ajude-nos</a>
+        </div>
+        <div class="menu-item">
+            <img src="assets/img/diretriz.png" alt="diretrizes">
+            <a href="#">Diretrizes</a>
+        </div>
+        <div class="menu-item">
+            <img src="assets/img/sobre.png" alt="sobre">
+            <a href="#">Sobre nós</a>
+        </div>
+        <div class="footer">Fulaninha</div>
+    </div>
+</body>
+</html>
